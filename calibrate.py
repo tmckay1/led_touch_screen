7727bcf2392e29f545fa8aa402f3a489323fb89b
Tyler McKay <tmckay1.jackson@gmail.com>
2021-05-27 00:26:45 -0400

    add calibrate

---
+++ calibrate.py
@@ -0,0 +1,11 @@
+from cursors.TouchCursor import TouchCursor
+import time
+
+device_path = "/dev/input/event1"
+cursor = TouchCursor(device_path)
+
+# script that will print out the x, y values of the current touch screen while a finger is on it
+while True:
+  (raw_x, raw_y) = cursor.get_current_position()
+  print("x: " + str(raw_x) + ", y: " + str(raw_y))
+  time.sleep(3)
