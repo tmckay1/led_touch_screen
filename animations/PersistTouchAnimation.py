c50eb6ec14fc327b1c3445c93a7084f9cd7ead94
Tyler McKay <tmckay1.jackson@gmail.com>
2021-05-28 00:17:50 -0400

    add debugging

--- animations/PersistTouchAnimation.py
+++ animations/PersistTouchAnimation.py
@@ -10,7 +10,11 @@ class PersistTouchAnimation(Animation):
     self._color = color
   
   def run(self):
-    raw_position = self._cursor.get_current_position()
-    (x, y) = self._cursor_config.get_x_y_position_from_raw_position(raw_position)
-    if x != -1 and y != -1:
-      self._led_matrix.set(x, y, self._color)
+    print("Running PersistTouchAnimation")
+    while True:
+      raw_position = self._cursor.get_current_position()
+      print("Retrieved raw_position: " + str(raw_position))
+      (x, y) = self._cursor_config.get_x_y_position_from_raw_position(raw_position)
+      print("x: " + str(x) + ", y: " + str(y))
+      if x != -1 and y != -1:
+        self._led_matrix.set(x, y, self._color)
